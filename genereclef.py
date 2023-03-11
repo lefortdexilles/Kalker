83e534968f179d09196a81bc3e76e77f5c78921f
renelefortdexilles <renelefortdexille@gmail.com>
2023-03-11 19:36:30 +0100

    chpassword10

---
+++ genereclef.py
@@ -0,0 +1,16 @@
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+noms = ("Pascale Gay", "Axel Nogue", "Moi")
+identifs = ("Pascale G", "Axel N", "Ego")
+
+passwords = ("abc123", "def789", "ihg456")
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hash-dutruc.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
+
